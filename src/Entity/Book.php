3e671f9ebff8a2a7e72908fa525c345dafe6788b
LAPTOP-UE4TQ2HP\verve <vervecken.hanne@gmail.com>
2023-05-12 14:51:38 +0200

    added query to get 20 books for page

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -44,14 +44,14 @@ class Book
         return $this->id;
     }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
     public function setId(?int $id): void
     {
         $this->id = $id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
     }
 
     public function setTitle(string $title): self
