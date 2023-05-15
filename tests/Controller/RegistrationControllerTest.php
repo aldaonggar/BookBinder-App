3f9d1ad890b9094d0cea9a657ba59be5f0432ca7
dias53 <101265345+dias53@users.noreply.github.com>
2023-05-15 16:44:40 +0200

    added test to check if not valid email is used

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -24,23 +24,41 @@ class RegistrationControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Email Verification');
     }
 
-    public function testNewUserRegistration(): void
+    public function testRegistrationWithValidData(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/register');
 
         $form = $crawler->selectButton('Register')->form();
-        $form['registration_form[email]'] = 'testuser@test.com';
+        $form['registration_form[email]'] = 'test@example.com';
         $form['registration_form[firstname]'] = 'Test';
         $form['registration_form[lastname]'] = 'User';
-        $form['registration_form[birthday]'] = '1990-01-01';
-        $form['registration_form[plainPassword][first]'] = 'Test123!';
-        $form['registration_form[plainPassword][second]'] = 'Test123!';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
         $form['registration_form[agreeTerms]'] = 1;
 
         $client->submit($form);
 
-        // The form submission should trigger a redirection to the email verification page
         $this->assertResponseRedirects('/email-verification');
+    }
+
+    public function testRegistrationWithInvalidEmail(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'not_an_email';
+        $form['registration_form[firstname]'] = 'Test';
+        $form['registration_form[lastname]'] = 'User';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        $this->assertSelectorTextContains('.form-error-message', 'Please enter a valid email address.');
     }
 }
