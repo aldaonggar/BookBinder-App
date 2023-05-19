f207391d67bec95b3d01ad724d2e14766977bc3c
dias53 <101265345+dias53@users.noreply.github.com>
2023-05-19 15:52:46 +0200

    added tests to check password constraints, duplicate email

--- tests/Unit/Controller/RegistrationControllerTest.php
+++ tests/Unit/Controller/RegistrationControllerTest.php
@@ -21,7 +21,7 @@ class RegistrationControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/register');
 
         $form = $crawler->selectButton('Register')->form();
-        $form['registration_form[email]'] = 'test3@example.com';
+        $form['registration_form[email]'] = 'testhello5353@gmail.com';
         $form['registration_form[firstname]'] = 'John';
         $form['registration_form[lastname]'] = 'Doe';
         $form['registration_form[birthday]'] = '2000-01-01';
@@ -32,6 +32,8 @@ class RegistrationControllerTest extends WebTestCase
         $client->submit($form);
 
         $this->assertTrue($client->getResponse()->isRedirect());
+        $expectedUrl = '/email-verification'; // replace this with the expected URL
+        $this->assertEquals($expectedUrl, $client->getResponse()->getTargetUrl());
         // You can add more assertions here to check that the data has been inserted correctly
     }
 
@@ -51,7 +53,7 @@ class RegistrationControllerTest extends WebTestCase
 
         $client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        //$this->assertFalse($client->getResponse()->isSuccessful());
         $this->assertFalse($client->getResponse()->isRedirect());
 
 
@@ -73,7 +75,7 @@ class RegistrationControllerTest extends WebTestCase
 
         $client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        //$this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertFalse($client->getResponse()->isRedirect());
         // You can add more assertions here to check that the data has been inserted correctly
 
@@ -85,17 +87,17 @@ class RegistrationControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/register');
 
         $form = $crawler->selectButton('Register')->form();
-        $form['registration_form[email]'] = 'invalid@gmail.com';
+        $form['registration_form[email]'] = 'invalid2@gmail.com';
         $form['registration_form[firstname]'] = 'John';
         $form['registration_form[lastname]'] = 'Doe';
-        $form['registration_form[birthday]'] = '2053-44-51';
+        $form['registration_form[birthday]'] = '2053-11-11';
         $form['registration_form[plainPassword][first]'] = 'Password123!';
-        $form['registration_form[plainPassword][second]'] = 'password';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
         $form['registration_form[agreeTerms]'] = 1;
 
         $client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        //$this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertFalse($client->getResponse()->isRedirect());
         // You can add more assertions here to check that the data has been inserted correctly
 
@@ -110,15 +112,160 @@ class RegistrationControllerTest extends WebTestCase
         $form['registration_form[email]'] = 'agree@gmail.com';
         $form['registration_form[firstname]'] = 'John';
         $form['registration_form[lastname]'] = 'Doe';
-        $form['registration_form[birthday]'] = '2053-44-51';
+        $form['registration_form[birthday]'] = '2001-12-12';
         $form['registration_form[plainPassword][first]'] = 'Password123!';
-        $form['registration_form[plainPassword][second]'] = 'password';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
 
         $client->submit($form);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        //$this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertFalse($client->getResponse()->isRedirect());
         // You can add more assertions here to check that the data has been inserted correctly
 
+    }
+
+    public function testExistingEmailRegister(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'testhello5353@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+
+    }
+
+    public function testPasswordLengthRegister(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'passwordemaiil@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'Pa123!';
+        $form['registration_form[plainPassword][second]'] = 'Pa123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+    }
+
+        public function testPasswordSpecialCharRegister(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'passwordemaiil@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'Password123';
+        $form['registration_form[plainPassword][second]'] = 'Password123';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+    }
+        public function testPasswordUpperCaseRegister(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'passwordemaiil@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'password123!';
+        $form['registration_form[plainPassword][second]'] = 'password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+    }
+        public function testPasswordLowerCaseRegister(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'passwordemaiil@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'PASSWORD123!';
+        $form['registration_form[plainPassword][second]'] = 'PASSWORD123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+    }
+
+    public function testPasswordDigitRegister(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'passwordemaiil@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'Password!';
+        $form['registration_form[plainPassword][second]'] = 'Password!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        //$this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertFalse($client->getResponse()->isRedirect());
+    }
+
+    public function testCorrectPasswordRegister(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'correctpassword53@gmail.com';
+        $form['registration_form[firstname]'] = 'Michael';
+        $form['registration_form[lastname]'] = 'Scofield';
+        $form['registration_form[birthday]'] = '1998-05-05';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $expectedUrl = '/email-verification'; // replace this with the expected URL
+        $this->assertEquals($expectedUrl, $client->getResponse()->getTargetUrl());
     }
 }
