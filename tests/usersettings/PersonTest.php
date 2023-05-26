617946051b16fedc7ca44657ae486d127214266f
sophie.haelters <sophie.haelters@student.kuleuven.be>
2023-05-26 13:43:55 +0200

    Al route tests done for person, myprofile en usersettings

--- tests/usersettings/PersonTest.php
+++ tests/usersettings/PersonTest.php
@@ -38,4 +38,20 @@ class PersonTest extends WebTestCase
         $crawler = $client->request('GET', '/person');
         $this->assertResponseIsSuccessful();
     }
+
+    public function testRouteUserSettings(): void {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        // Submit the form with valid credentials
+        $form = $crawler->selectButton('Login')->form([
+            '_username' => 'sophie@haelters.be',
+            '_password' => 'Blabla1!',
+        ]);
+        $client->submit($form);
+
+        $crawler = $client->request('GET', '/usersettings');
+        $this->assertResponseIsSuccessful();
+    }
 }
