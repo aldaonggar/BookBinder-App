6308264cdabf66f7a5c80e73e84b97e4bb1e2fcd
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-05-26 11:36:41 +0200

    each book page has description from google API, also a form for functional search is added

--- src/Controller/BookBinderController.php
+++ src/Controller/BookBinderController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\SearchFormType;
 use Doctrine\ORM\EntityManagerInterface;
+//use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\ExtraClasses\BookGeneratorForTests;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookBinderController extends AbstractController
 {
@@ -15,16 +18,29 @@ class BookBinderController extends AbstractController
 //     * @Route("/booklist.html.twig")
 //     */
 // comment test
-    public function renderBookList(EntityManagerInterface $entityManager, int $page){
+    public function renderBookList(EntityManagerInterface $entityManager, int $page, Request $request){
         $repository = $entityManager->getRepository(Book::class);
         $books = $repository->get21Books($page);
-
-        //$bookGenerator = new BookGeneratorForTests($page);
         $numberOfPages = ceil(($repository->getNumberOfBooks())/21);
+
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchTerm = $form->getData()['searchTerm'];
+
+            // Perform search logic with the $searchTerm
+            // ...
+
+            //return $this->redirectToRoute('search_results', ['searchTerm' => $searchTerm]);
+            return $this->redirectToRoute('search', ['searchTerm' => $searchTerm]);
+        }
+
         return $this->render('booklist.html.twig', [
             'bookArray'=>$books,
             'numberOfPages'=> $numberOfPages,
-            'currentPage'=>$page
+            'currentPage'=>$page,
+            'form'=>$form->createView(),
         ]);
 
         /*
@@ -33,6 +49,32 @@ class BookBinderController extends AbstractController
          * $bookGenerator = new BookGeneratorForTests();
         $stringResponse = $bookGenerator->createStringResponse();
         return new Response($stringResponse);*/
+    }
+
+    public function renderBookListSearch(EntityManagerInterface $entityManager, string $searchTerm, Request $request){
+        $repository = $entityManager->getRepository(Book::class);
+        $books = $repository->get21Books(3);
+        $numberOfPages = ceil(($repository->getNumberOfBooks())/21);
+
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchTerm = $form->getData()['searchTerm'];
+
+            // Perform search logic with the $searchTerm
+            // ...
+
+            //return $this->redirectToRoute('search_results', ['searchTerm' => $searchTerm]);
+            return $this->redirectToRoute('search', ['searchTerm' => $searchTerm]);
+        }
+
+        return $this->render('booklist.html.twig', [
+            'bookArray'=>$books,
+            'numberOfPages'=> $numberOfPages,
+            'currentPage'=>3,
+            'form'=>$form->createView(),
+        ]);
     }
 
     public function renderBook(EntityManagerInterface $entityManager, int $id){
