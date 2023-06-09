451a88e36ccd9510a897aa5cfb489944f5e2dcfd
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-06-09 19:39:44 +0200

    deleting library list in homepage, deleting filler description in booklist page
    ensuring tests works 100%: in testing the favourite book you just get a book from the db instead of creating one plus fixing some small bugs

--- tests/Integration/Controller/BookControllerTest.php
+++ tests/Integration/Controller/BookControllerTest.php
@@ -2,15 +2,22 @@
 
 namespace App\Tests\Integration\Controller;
 
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BookControllerTest extends WebTestCase
 {
+    private $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
     public function testBookListPage()
     {
-        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/booklist/1');
+
+        $crawler = $this->client->request('GET', '/booklist/1');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('head title', 'Book List');
@@ -28,7 +35,7 @@ class BookControllerTest extends WebTestCase
         // Assert that at least one element matching the selector contains the text "Harry Potter"
         $this->assertTrue($containsHarryPotter, 'No element contains the text "Harry Potter"');
 
-        $crawler = $client->request('GET', '/booklist/6');
+        $crawler = $this->client->request('GET', '/booklist/6');
         $elements = $crawler->filter('h5.card-title');
 
 
@@ -42,61 +49,61 @@ class BookControllerTest extends WebTestCase
         // Assert that at least one element matching the selector contains the text "Harry Potter"
         $this->assertTrue($containsTitle, 'No element contains the text "Thinking, fast and slow"');
 
-        $crawler = $client->request('GET', '/booklist/8');
+        $crawler = $this->client->request('GET', '/booklist/8');
         $this->assertSelectorTextContains('h2', 'Book page is not available');
 
-        $crawler = $client->request('GET', '/booklist/0');
+        $crawler = $this->client->request('GET', '/booklist/0');
         $this->assertSelectorTextContains('h2', 'Book page is not available');
     }
 
     public function testBookPage():void {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+
+        $crawler = $this->client->request('GET', '/login');
 
         $form = $crawler->selectButton('Login')->form([
             '_username' => 'yeska.omar@gmail.com',
             '_password' => 'Hello123.',
         ]);
-        $client->submit($form);
+        $this->client->submit($form);
 
         // Assert the user was redirected to the homepage or wherever your app redirects on successful login
         $this->assertResponseRedirects('http://localhost/home');
 
         //test if it can properly fetch a book with the right ID
-        $client->request('GET', '/book/5');
+        $this->client->request('GET', '/book/5');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'The Hobbit');
         $this->assertSelectorTextContains('h4', 'J.R.R. Tolkien');
 
         //test if it can detect non existing book
-        $client->request('GET', '/book/0');
+        $this->client->request('GET', '/book/0');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', "There's no book of specified ID :(");
 
-        $client->request('GET', '/book/2000');
+        $this->client->request('GET', '/book/2000');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', "There's no book of specified ID :(");
 
     }
 
     public function testSearchPage():void{
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+
+        $crawler = $this->client->request('GET', '/login');
 
         $form = $crawler->selectButton('Login')->form([
             '_username' => 'yeska.omar@gmail.com',
             '_password' => 'Hello123.',
         ]);
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $crawler = $client->request('GET', '/booklist/1');
+        $crawler = $this->client->request('GET', '/booklist/1');
 
         $form = $crawler->filter('form[name="search_form"]')->form();
         $form['search_form[searchTerm]'] = 'harry';
-        $crawler = $client->submit($form);
+        $crawler = $this->client->submit($form);
         $this->assertResponseRedirects('/booklist/search/harry');
 
-        $crawler = $client->request('GET', '/booklist/search/harry');
+        $crawler = $this->client->request('GET', '/booklist/search/harry');
         $elements = $crawler->filter('h5.card-title');
         $containsTitle = false;
         foreach ($elements as $element) {
@@ -110,9 +117,207 @@ class BookControllerTest extends WebTestCase
 
         $form = $crawler->filter('form[name="search_form"]')->form();
         $form['search_form[searchTerm]'] = 'hello';
-        $crawler = $client->submit($form);
+        $crawler = $this->client->submit($form);
         $this->assertResponseRedirects('/booklist/search/hello');
-        $crawler = $client->request('GET', '/booklist/search/hello');
+        $crawler = $this->client->request('GET', '/booklist/search/hello');
         $this->assertSelectorTextContains('h2', 'No books corresponding to your search :(');
+    }
+
+    public function testRoute() {
+        $email = 'test' . uniqid() . '@example.com';
+
+        // Request the registration page
+        $crawler = $this->client->request('GET', '/register');
+
+        // Fill in the registration form
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = $email;
+        $form['registration_form[firstname]'] = 'John';
+        $form['registration_form[lastname]'] = 'Doe';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+        $this->client->submit($form);
+
+        $crawler = $this->client->request('GET', '/login');
+
+        // Submit the form with valid credentials
+        $form = $crawler->selectButton('Login')->form([
+            '_username' => $email,
+            '_password' => 'Password123!',
+        ]);
+        $this->client->submit($form);
+
+        $book = $this->getBookFromDatabaseBetter();
+        $crawler = $this->client->request('GET', '/book/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testSetFavorite() {
+        $email = 'test' . uniqid() . '@example.com';
+
+        // Request the registration page
+        $crawler = $this->client->request('GET', '/register');
+
+        // Fill in the registration form
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = $email;
+        $form['registration_form[firstname]'] = 'John';
+        $form['registration_form[lastname]'] = 'Doe';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+        $this->client->submit($form);
+
+        $crawler = $this->client->request('GET', '/login');
+
+        // Submit the form with valid credentials
+        $form = $crawler->selectButton('Login')->form([
+            '_username' => $email,
+            '_password' => 'Password123!',
+        ]);
+        $this->client->submit($form);
+
+        $book = $this->getBookFromDatabaseBetter();
+        $crawler = $this->client->request('GET', '/book/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertFalse($data['isFavorite']); // Assert that the book is initially favorited
+    }
+
+    public function testToggleFavoriteToTrue() {
+        $email = 'test' . uniqid() . '@example.com';
+
+        // Request the registration page
+        $crawler = $this->client->request('GET', '/register');
+
+        // Fill in the registration form
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = $email;
+        $form['registration_form[firstname]'] = 'John';
+        $form['registration_form[lastname]'] = 'Doe';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+        $this->client->submit($form);
+
+        $crawler = $this->client->request('GET', '/login');
+
+        // Submit the form with valid credentials
+        $form = $crawler->selectButton('Login')->form([
+            '_username' => $email,
+            '_password' => 'Password123!',
+        ]);
+        $this->client->submit($form);
+
+        $book = $this->getBookFromDatabaseBetter();
+        $crawler = $this->client->request('GET', '/book/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertFalse($data['isFavorite']); // Assert that the book is initially favorited
+
+        $this->client->request('POST', '/toggle-favorite/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertTrue($data['isFavorite']); // Assert that the book is unfavorited
+
+        $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertTrue($data['isFavorite']); // Assert that the book is unfavorited
+    }
+
+    public function testToggleFavoriteToFalse() {
+        $email = 'test' . uniqid() . '@example.com';
+
+        // Request the registration page
+        $crawler = $this->client->request('GET', '/register');
+
+        // Fill in the registration form
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = $email;
+        $form['registration_form[firstname]'] = 'John';
+        $form['registration_form[lastname]'] = 'Doe';
+        $form['registration_form[birthday]'] = '2000-01-01';
+        $form['registration_form[plainPassword][first]'] = 'Password123!';
+        $form['registration_form[plainPassword][second]'] = 'Password123!';
+        $form['registration_form[agreeTerms]'] = 1;
+        $this->client->submit($form);
+
+        $crawler = $this->client->request('GET', '/login');
+
+        // Submit the form with valid credentials
+        $form = $crawler->selectButton('Login')->form([
+            '_username' => $email,
+            '_password' => 'Password123!',
+        ]);
+        $this->client->submit($form);
+
+        $book = $this->getBookFromDatabaseBetter();
+        $crawler = $this->client->request('GET', '/book/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertFalse($data['isFavorite']); // Assert that the book is initially favorited
+
+        $this->client->request('POST', '/toggle-favorite/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertTrue($data['isFavorite']); // Assert that the book is unfavorited
+
+        $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertTrue($data['isFavorite']); // Assert that the book is unfavorited
+
+        $this->client->request('POST', '/toggle-favorite/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertFalse($data['isFavorite']); // Assert that the book is unfavorited
+
+        $this->client->request('GET', '/get-favorite-status/' . $book->getId());
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertFalse($data['isFavorite']); // Assert that the book is unfavorited
+    }
+
+    private function getBookFromDatabase()
+    {
+        $entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $book = new Book();
+        $book->setId(999);
+        $book->setTitle('Book Title');
+        $book->setAuthor('Author Name');
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $book;
+    }
+
+    private function getBookFromDatabaseBetter(){
+        // Generate a random book ID between 1 and 110
+        $randomId = rand(1, 110);
+        $entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Fetch the book with the random ID from the database
+        $book = $entityManager->getRepository(Book::class)->find($randomId);
+
+        if (!$book) {
+            throw new \Exception('Book not found');
+        }
+        return $book;
     }
 }
